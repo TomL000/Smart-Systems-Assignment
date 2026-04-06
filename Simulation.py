ea1a8fb97e996328c2c7113d314b4eb48a8d4a27
Tom <146028728+TomL000@users.noreply.github.com>
2026-04-06 18:12:40 +0100

    Thresholds and fetch from sql

--- Simulation.py
+++ Simulation.py
@@ -2,6 +2,9 @@ import sqlite3
 import random
 import time
 
+humidityThresholdHigh = 70
+humidityThresholdLow = 35
+
 def genHumidity():
     return round (random.uniform(30, 80), 2)
 
@@ -22,6 +25,17 @@ for _ in range(20):
     sqlCursor.execute("INSERT INTO humidity_reading (humidity) VALUES (?)", (humidity,))
     sqlConnection.commit()
 
-    print (f"recorded humidity {humidity}%")
+    print (f"Recorded humidity: {humidity}%")
+    if humidity > humidityThresholdHigh:
+        print(f"WARNING High Humidity Alert: {humidity}%")
+    elif humidity < humidityThresholdLow:
+        print(f"WARNING Low Humidity Alert: {humidity}%")
     time.sleep(1)
+
+sqlCursor.execute("SELECT * FROM humidity readings")
+sqlRows = sqlCursor.fetchall()
+
+for row in sqlRows:
+    print(row)
+
 sqlConnection.close()
