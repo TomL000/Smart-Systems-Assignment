f0e961e602cb7c81a783949ecb82ceca0b3f4f41
Tom <146028728+TomL000@users.noreply.github.com>
2026-04-06 18:06:35 +0100

    humidity update

--- Simulation.py
+++ Simulation.py
@@ -8,11 +8,19 @@ def genHumidity():
 sqlConnection = sqlite3.connect("humidity.db")
 sqlCursor = sqlConnection.cursor()
 
+sqlCursor.execute(""
+"""CREATE TABLE IF NOT EXISTS humidity_readings (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    humidity REAL
+    )"""
+)
+
 for _ in range(20):
     humidity = genHumidity()
 
-    # cursor.execute("INSERT INTO humidity_reading (humidity) VALUES (?)", (humidity,))
-    # sqlConnection.commit()
+    sqlCursor.execute("INSERT INTO humidity_reading (humidity) VALUES (?)", (humidity,))
+    sqlConnection.commit()
 
     print (f"recorded humidity {humidity}%")
     time.sleep(1)
