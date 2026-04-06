57b654d31bf76abd3bda081503c2d455db5fd456
Tom <146028728+TomL000@users.noreply.github.com>
2026-04-06 18:22:49 +0100

    comments

--- Simulation.py
+++ Simulation.py
@@ -2,16 +2,22 @@ import sqlite3
 import random
 import time
 
-# Thresholds for a high and low 
+# Thresholds for a high and low temperatures
 
 humidityThresholdHigh = 70
 humidityThresholdLow = 35
+ 
+# Defines a humidity generator
 
 def genHumidity():
     return round (random.uniform(30, 80), 2)
 
+# Connection to sql database
+
 sqlConnection = sqlite3.connect("humidity.db")
 sqlCursor = sqlConnection.cursor()
+
+# Sql cursor to insert a table into the SQL database that stores the humidity readings.
 
 sqlCursor.execute(
 """CREATE TABLE IF NOT EXISTS humidity_readings (
@@ -20,6 +26,8 @@ sqlCursor.execute(
     humidity REAL
     )"""
 )
+
+# Live while loop that executes the humidity readings and compares them into the threshholds
 
 while True:
     humidity = genHumidity()
@@ -32,8 +40,9 @@ while True:
         print(f"WARNING High Humidity Alert: {humidity}%")
     elif humidity < humidityThresholdLow:
         print(f"WARNING Low Humidity Alert: {humidity}%")
-    time.sleep(2)
+    time.sleep(30)
 
+# fetches from the sql table and prints in a row from this file.
 sqlCursor.execute("SELECT * FROM humidity readings")
 sqlRows = sqlCursor.fetchall()
 
