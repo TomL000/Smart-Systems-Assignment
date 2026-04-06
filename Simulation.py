5f0853c2fb6320f319424f8e3905743ad116d2c7
Tom <146028728+TomL000@users.noreply.github.com>
2026-04-06 18:15:49 +0100

    Live dashboard

--- Simulation.py
+++ Simulation.py
@@ -11,7 +11,7 @@ def genHumidity():
 sqlConnection = sqlite3.connect("humidity.db")
 sqlCursor = sqlConnection.cursor()
 
-sqlCursor.execute(""
+sqlCursor.execute(
 """CREATE TABLE IF NOT EXISTS humidity_readings (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
@@ -19,18 +19,18 @@ sqlCursor.execute(""
     )"""
 )
 
-for _ in range(20):
+while True:
     humidity = genHumidity()
 
     sqlCursor.execute("INSERT INTO humidity_reading (humidity) VALUES (?)", (humidity,))
     sqlConnection.commit()
 
-    print (f"Recorded humidity: {humidity}%")
+    print (f"[LIVE] Recorded humidity: {humidity}%")
     if humidity > humidityThresholdHigh:
         print(f"WARNING High Humidity Alert: {humidity}%")
     elif humidity < humidityThresholdLow:
         print(f"WARNING Low Humidity Alert: {humidity}%")
-    time.sleep(1)
+    time.sleep(2)
 
 sqlCursor.execute("SELECT * FROM humidity readings")
 sqlRows = sqlCursor.fetchall()
