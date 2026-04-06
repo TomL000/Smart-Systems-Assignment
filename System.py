c2e3b408ea0a8d0826f09f4a667e9839bb4272c7
Tom <146028728+TomL000@users.noreply.github.com>
2026-04-06 17:47:26 +0100

    Update System.py

--- System.py
+++ System.py
@@ -1,1 +1,14 @@
+# Importing the SQL library
+import sqlite3
 
+# Simple SQL connection to humidity.db
+sqlConnection = sqlite3.connect('humidity.db')
+sqlCursor = sqlConnection.cursor()
+
+# sqlCursor.execute()
+
+sqlConnection.commit()
+sqlConnection.close()
+
+name = "Name"
+time = "12:00"
