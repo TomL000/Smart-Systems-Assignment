8b08e8cf00b965840d51488fa3bababcf4a6325c
Harry <hp315@canterbury.ac.uk>
2026-04-13 20:30:18 +0100

    revised main.py to only call frontend.py. Test = OK

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
 # importing libraries
-from presentation import userMenu
-import time
 import datetime
 import sqlite3
-from processing import humidityMonitor
 #H#
 import json #add json integration
-#add actuation
-from actuation import actuate
-#adding simulation layer start-up
-import threading
-import Simulation #DEV ONLY - simulates deployed sensor feed
-
-def startSimulation():
-    # DEV ONLY - Launches simulation as background thread to seed database with readings
-    dev_thread = threading.Thread(target=Simulation.run, daemon=True)
-    dev_thread.start()
-    time.sleep(12) #allow simulation to populate
-    #H#
+from frontend import main as startFrontend
 
 # main code sequence
 def main():
@@ -59,28 +45,8 @@ def main():
     sqlConnection.commit()
     sqlConnection.close()
 
-    userMenu()
-    startSimulation()
-    print("Press CTRL+C to pause humidity monitoring and return to menu.")
-    # loops humidity monitoring function until user hits CTRL-C to return to menu, where they may quit, adjust or continue.
-    while True:
-        try:
-            humidityMonitor()
-            actuate()
-            time.sleep(30)
-        except KeyboardInterrupt:
-            print("\nINTERRUPT\n")
-            sqlConnection = sqlite3.connect('humidity.db')
-            sqlCursor = sqlConnection.cursor()
-            sqlCursor.execute("""
-            INSERT INTO logActions (action, timestamp)
-            VALUES (?, ?);
-            """, (
-                "Interrupt", datetime.datetime.now().isoformat()
-            ))
-            sqlConnection.commit()
-            sqlConnection.close()
-            userMenu()
+    #launches frontend.py, which handels simulation, processing, actuation, and presentation layers
+    startFrontend()
 
 # executes main code
 main()
