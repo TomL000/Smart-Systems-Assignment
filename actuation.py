b6ccd66ccfdc416beeca66061cea4ea991d1f2e7
Harry <hp315@canterbury.ac.uk>
2026-04-13 03:01:29 +0100

    added energy consumption values (Watts/KWH) to each actuation and log. Test = OK

--- actuation.py
+++ actuation.py
@@ -6,6 +6,13 @@ import datetime
 import json
 from processing import zoneComparison
 
+#wattage constants
+watt_system_base = 10
+watt_humidifier = 50
+watt_dehumidifier = 40
+watt_idle = 5
+interval_seconds = 30 #matches main.py sleep interval
+
 #actuation function
 def actuate():
     #connect to database
@@ -29,6 +36,20 @@ def actuate():
     );
     """)
 
+    #create wattage table if non-existant
+    sqlCursor.execute("""
+    CREATE TABLE IF NOT EXISTS logEnergy (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        timestamp TEXT,
+        zoneNum TEXT,
+        action TEXT,
+        wattsUsed REAL,
+        energyKwh REAL
+    );
+    """)
+
+    totalWatts = watt_system_base
+
     #actuation process for each reading
     for zoneNum, comparison in zoneComparison.items():
         #recieve zone information and transfer to 'zoneName' variable
@@ -38,20 +59,35 @@ def actuate():
         if comparison == "<":
             action = "HUMIDIFER ON"
             reason = f"{zoneName} humidity below threshold"
+            watts = watt_humidifier
         elif comparison == ">":
             action = "DEHUMIDIFIER ON"
             reason = f"{zoneName} humidity above range"
+            watts = watt_dehumidifier
         else:
             action = "IDLE"
             reason = f"{zoneName} humidity within range"
+            watts = watt_idle
+        
+        totalWatts += watts
+        #convert watt seconds into kilowatt hours
+        energyKwh = round ((watts * interval_seconds) / 3600000, 6)
         
         print(f"[ACTUATION] Zone {zoneNum}: {action} | {reason}")
 
-        #log into sql
+        #log actuation into sql
         sqlCursor.execute("""
         INSERT INTO logActuation (zoneNum, action, reason, timestamp)
         VALUES (?, ?, ?, ?)
         """, (zoneNum, action, reason, datetime.datetime.now().isoformat())) #convert to string
+
+        #log watts into sql
+        sqlCursor.execute("""
+        INSERT INTO logEnergy (timestamp, zoneNum, action, wattsUsed, energyKwh)
+        VALUES (?, ?, ?, ?, ?)
+        """, (datetime.datetime.now().isoformat(), zoneNum, action, watts, energyKwh))
+
+    print(f"[ENERGY] Total system draw: {totalWatts}W")
 
     sqlConnection.commit()
     sqlConnection.close()
